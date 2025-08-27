1715bbb78766120190114bbebd8b6961b556105f
Laura Jimenez <laufjimdev@gmail.com>
2025-08-26 21:31:09 -0400

    mealsSlice and store config

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,10 +4,14 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    { name: 'Breakfast', cost: 50, selected: false },
+    { name: 'High Tea', cost: 25, selected: false },
+    { name: 'Lunch', cost: 65, selected: false },
+    { name: 'Dinner', cost: 70, selected: false },
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+        state[action.payload].selected = !state[action.payload].selected;
     },
   },
 });
@@ -15,3 +19,4 @@ export const mealsSlice = createSlice({
 export const { toggleMealSelection } = mealsSlice.actions;
 
 export default mealsSlice.reducer;
+
